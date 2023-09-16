41c5b8582cc86edcea07aa1aa7f5e7c9dc12e218
jekovniki <jekovniki95@gmail.com>
2023-09-16 08:11:53 +0300

    Starting setting the footer

--- wordpress/wp-content/themes/levskisofia/index.php
+++ wordpress/wp-content/themes/levskisofia/index.php
@@ -10,8 +10,10 @@
     
     <header>
         <div class="header-line"></div>
-        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-        <p><?php bloginfo( 'description' ); ?></p>
+        <div class="section">
+            <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+            <p><?php bloginfo( 'description' ); ?></p>
+        </div>
     </header>
     
     <nav>
@@ -26,27 +28,43 @@
     </nav>
     
     <main>
-        <?php
-        if ( have_posts() ) :
-            while ( have_posts() ) :
-                the_post();
+        <div class="section">
+            <?php
+            if ( have_posts() ) :
+                while ( have_posts() ) :
+                    the_post();
+                    ?>
+                    <article <?php post_class(); ?>>
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <div class="post-content">
+                            <?php the_content(); ?>
+                        </div>
+                    </article>
+                    <?php
+                endwhile;
+            else :
                 ?>
-                <article <?php post_class(); ?>>
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <div class="post-content">
-                        <?php the_content(); ?>
-                    </div>
-                </article>
-                <?php
-            endwhile;
-        else :
-            ?>
-            <p><?php _e( 'Sorry, no posts found.', 'your-text-domain' ); ?></p>
-        <?php endif; ?>
+                <p><?php _e( 'Sorry, no posts found.', 'your-text-domain' ); ?></p>
+            <?php endif; ?>
+        </div>
     </main>
     
     <footer>
-        <p>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></p>
+        <div class="legal-panel">
+            <div class="footer-wrapper">
+                <nav>
+                    <?php
+                    // Display the WordPress navigation menu
+                    wp_nav_menu( array(
+                        'theme_location' => 'secondary-menu', // You should define this in your functions.php file
+                        'container'      => false, // Remove the outer div container
+                        'menu_class'     => 'menu', // CSS class for the ul element
+                    ) );
+                    ?>
+                </nav>
+                <p class="copyright">&copy; <?php echo date( 'Y' ); ?> ХК Левски София. Всички права запазени. Никаква част от този сайт не може да бъде възпроизвеждана без нашето писмено разрешение. </p>
+            </div>
+        </div>
     </footer>
     
     <?php wp_footer(); ?>

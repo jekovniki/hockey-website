3754882dc2fb425aee315973fc818f3143d602ff
jekovniki <jekovniki95@gmail.com>
2023-09-19 23:07:06 +0300

    Remove

--- wordpress/wp-content/themes/levskisofia/index.php
+++ wordpress/wp-content/themes/levskisofia/index.php
@@ -12,40 +12,74 @@
         <div class="section">
             <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
             <p><?php bloginfo( 'description' ); ?></p>
+            <div class="navigation">
+                <nav>
+                    <?php
+                    // Display the WordPress navigation menu
+                    wp_nav_menu( array(
+                        'theme_location' => 'primary-menu', // You should define this in your functions.php file
+                        'container'      => false, // Remove the outer div container
+                        'menu_class'     => 'menu', // CSS class for the ul element
+                    ) );
+                    ?>
+                </nav>
+            </div>
         </div>
     </header>
-    
-    <nav>
-        <?php
-        // Display the WordPress navigation menu
-        wp_nav_menu( array(
-            'theme_location' => 'primary-menu', // You should define this in your functions.php file
-            'container'      => false, // Remove the outer div container
-            'menu_class'     => 'menu', // CSS class for the ul element
-        ) );
-        ?>
-    </nav>
-    
     <main>
-        <div class="section">
-            <?php
-            if ( have_posts() ) :
-                while ( have_posts() ) :
-                    the_post();
-                    ?>
-                    <article <?php post_class(); ?>>
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <div class="post-content">
-                            <?php the_content(); ?>
-                        </div>
-                    </article>
-                    <?php
-                endwhile;
-            else :
+    <section id="news">
+        <?php
+        $args = array(
+            'category_name' => 'news', // Replace 'news' with the slug of your 'news' category.
+            'posts_per_page' => -1,    // Display all posts from the category.
+        );
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) :
+            while ($query->have_posts()) :
+                $query->the_post();
                 ?>
-                <p><?php _e( 'Sorry, no posts found.', 'your-text-domain' ); ?></p>
-            <?php endif; ?>
+                <article <?php post_class(); ?>>
+    <a href="<?php the_permalink(); ?>">
+        <div class="post-content">
+            <?php
+            // Retrieve the "staging_images" field from the "News" ACF field group.
+            $staging_image = get_field('staging_images');
+
+            // Check if the field has a value.
+            if ($staging_image) {
+                echo '<img src="' . esc_url($staging_image['url']) . '" class="staging" alt="' . esc_attr($staging_image['alt']) . '" />';
+            }
+
+            // Get the post content and split it into an array of words.
+            $content = get_the_content();
+            $content_words = explode(' ', $content);
+
+            // Check if there are more than 50 words.
+            if (count($content_words) > 50) {
+                // Display the first 50 words.
+                $excerpt = implode(' ', array_slice($content_words, 0, 50));
+                echo wpautop($excerpt); // Ensure proper paragraph formatting.
+                ?>
+                <p><a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a></p>
+                <?php
+            } else {
+                // Display the full content if it has 50 words or less.
+                the_content();
+            }
+            ?>
         </div>
+    </a>
+</article>
+                <?php
+            endwhile;
+            wp_reset_postdata(); // Restore the global post data.
+        else :
+            ?>
+            <p><?php _e('Sorry, no posts found in the "News" category.', 'your-text-domain'); ?></p>
+        <?php endif; ?>
+    </section>
     </main>
     <footer>
         <div class="top">
@@ -84,6 +118,9 @@
                                 echo '<p>' . esc_html($footer_town) . '</p>';
                             }
                             ?>
+                            <div class="mt-2 mb-2">
+                            <i class="fab fa-facebook-f fa-2x" style="color: #3b5998;"></i>
+                            </div>
                         </div>
                         <div class="col-md-4">
                             <nav class="club-menu ml-1">

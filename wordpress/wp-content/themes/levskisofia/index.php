a43efde349c19e1134c3401afc2b9db7d682789a
jekovniki <jekovniki95@gmail.com>
2023-09-19 21:21:11 +0300

    Implement bootstrap

--- wordpress/wp-content/themes/levskisofia/index.php
+++ wordpress/wp-content/themes/levskisofia/index.php
@@ -47,72 +47,85 @@
             <?php endif; ?>
         </div>
     </main>
-    
     <footer>
-        <div class="legal-panel">
-            <div class="club-identity">
-                <div class="section">
-                <?php
-                    $footer_image = get_theme_mod('footer_image', '');
-                    if (!empty($footer_image)) {
-                        echo '<img src="' . esc_url($footer_image) . '" alt="Footer Image">';
-                    }
-                ?>
-                </div>
-                <div class="footer-wrapper">
-                    <div class="club-information">
-                    <?php
-                        $footer_name = get_theme_mod('footer_name', '');
-                        $footer_location = get_theme_mod('footer_location', '');
-                        $footer_arena = get_theme_mod('footer_arena', '');
-                        $footer_town = get_theme_mod('footer_town', '');
-                        if (!empty($footer_name)) {
-                            echo '<p><strong>' . esc_html($footer_name) . '</strong></p>';
-                        }
-                    
-                        if (!empty($footer_location)) {
-                            echo '<p>' . esc_html($footer_location) . '</p>';
-                        }
-                    
-                        if (!empty($footer_arena)) {
-                            echo '<p>' . esc_html($footer_arena) . '</p>';
-                        }
-                    
-                        if (!empty($footer_town)) {
-                            echo '<p>' . esc_html($footer_town) . '</p>';
-                        }
-                    ?>
-                    </div>
-                    <div class="club-navi">
-                        <nav>
+        <div class="top">
+            <div class="container">
+                <div class="row pt-4">
+                    <div class="col-md-12 club-identity">
+                        <div class="section">
                             <?php
-                            // Display the WordPress navigation menu
-                            wp_nav_menu( array(
-                                'theme_location' => 'club-menu', // You should define this in your functions.php file
-                                'container'      => false, // Remove the outer div container
-                                'menu_class'     => 'club-menu', // CSS class for the ul element
-                            ) );
+                            $footer_image = get_theme_mod('footer_image', '');
+                            if (!empty($footer_image)) {
+                                echo '<img src="' . esc_url($footer_image) . '" alt="Footer Image" class="img-fluid">';
+                            }
                             ?>
-                        </nav>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-md-4 left-info">
+                            <?php
+                            $footer_name = get_theme_mod('footer_name', '');
+                            $footer_location = get_theme_mod('footer_location', '');
+                            $footer_arena = get_theme_mod('footer_arena', '');
+                            $footer_town = get_theme_mod('footer_town', '');
+                            if (!empty($footer_name)) {
+                                echo '<p><strong>' . esc_html($footer_name) . '</strong></p>';
+                            }
+
+                            if (!empty($footer_location)) {
+                                echo '<p>' . esc_html($footer_location) . '</p>';
+                            }
+
+                            if (!empty($footer_arena)) {
+                                echo '<p>' . esc_html($footer_arena) . '</p>';
+                            }
+
+                            if (!empty($footer_town)) {
+                                echo '<p>' . esc_html($footer_town) . '</p>';
+                            }
+                            ?>
+                        </div>
+                        <div class="col-md-4">
+                            <nav class="club-menu ml-1">
+                                <?php
+                                // Display the WordPress navigation menu
+                                wp_nav_menu(array(
+                                    'theme_location' => 'club-menu', // You should define this in your functions.php file
+                                    'container' => false, // Remove the outer div container
+                                    'menu_class' => 'nav',
+                                ));
+                                ?>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="footer-wrapper">
-                <nav>
-                    <?php
-                    // Display the WordPress navigation menu
-                    wp_nav_menu( array(
-                        'theme_location' => 'secondary-menu', // You should define this in your functions.php file
-                        'container'      => false, // Remove the outer div container
-                        'menu_class'     => 'secondary-menu', // CSS class for the ul element
-                    ) );
-                    ?>
-                </nav>
-                <p class="copyright">&copy; <?php echo date( 'Y' ); ?> ХК Левски София. Всички права запазени. Никаква част от този сайт не може да бъде възпроизвеждана без нашето писмено разрешение. </p>
+        </div>
+        <div class="bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row pt-3">
+                            <nav>
+                                <?php
+                                // Display the WordPress navigation menu
+                                wp_nav_menu(array(
+                                    'theme_location' => 'secondary-menu', // You should define this in your functions.php file
+                                    'container' => false, // Remove the outer div container
+                                    'menu_class' => 'nav',
+                                ));
+                                ?>
+                            </nav>
+                        </div>
+                        <div class="row pt-3">
+                            <p class="copyright">&copy; <?php echo date('Y'); ?> ХК Левски София. Всички права запазени. Никаква част от този сайт не може да бъде възпроизвеждана без нашето писмено разрешение.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
-    
+
     <?php wp_footer(); ?>
 </body>
 </html>
